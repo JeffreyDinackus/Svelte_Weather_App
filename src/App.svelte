358f76b406733cd01537c112d7c94b80c8b5c1ca
JeffreyDinackus <jeffreydinackus@proton.me>
2024-01-20 15:10:15 -0500

    weather forecast is working

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,32 @@
   let responseData;
   let state;
   let features;
+  let longitude;
+  let latitude;
+  let weather2;
+  let forecase2;
+  let periods;
+
+  async function weather() {
+    try {
+      const response = await fetch(
+        `https://api.weather.gov/points/${latitude},${longitude}`,
+      );
+      const data = await response.json();
+      responseData = data;
+
+      console.log(responseData);
+      weather2 = responseData.properties;
+      let api = weather2.forecast;
+      const response2 = await fetch(`${api}`);
+      const data2 = await response2.json();
+
+      console.log(data2);
+      periods = data2.properties.periods;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
   async function getData() {
     try {
@@ -32,10 +58,29 @@
   <button on:click={getData}>Get Alerts</button>
 
   <br />
-  <textarea name="1" id="box2" cols="30" rows="2"></textarea><br />
+  <p>enter latitude</p>
+  <textarea name="1" id="box3" bind:value={latitude} cols="30" rows="2"
+  ></textarea>
+  <p>enter longitude</p>
+  <textarea name="1" id="box2" bind:value={longitude} cols="30" rows="2"
+  ></textarea><br />
 
-  <textarea name="1" id="box3" cols="30" rows="2"></textarea>
   <br />
+
+  <button on:click={weather}>Get Weather</button>
+
+  {#if periods}
+    {#each Object.entries(periods) as [title, paragraph]}
+      <h1>{title}</h1>
+      <p>Time: {paragraph.name}</p>
+
+      <p>When: {paragraph.detailedForecast}</p>
+      <p>Temperature: {paragraph.temperature}</p>
+    {/each}
+
+    <!-- <pre>{JSON.stringify(features, null, 2)}</pre> -->
+  {/if}
+
   {#if features}
     {#each Object.entries(features) as [title, paragraph]}
       <h1>{title}</h1>
